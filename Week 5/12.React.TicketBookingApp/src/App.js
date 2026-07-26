299afde4af0fd130783c9bbb35cc44e61e311593
chandanaer <chandanaer9@gmail.com>
2026-07-26 23:56:36 +0530

    Add files via upload

---
+++ Week 5/12.React.TicketBookingApp/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Greeting from "./Greeting";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function handleLoginClick() {
+    setIsLoggedIn(true);
+  }
+
+  function handleLogoutClick() {
+    setIsLoggedIn(false);
+  }
+
+  let button;
+
+  if (isLoggedIn) {
+    button = <LogoutButton onClick={handleLogoutClick} />;
+  } else {
+    button = <LoginButton onClick={handleLoginClick} />;
+  }
+
+  return (
+    <div>
+      <Greeting isLoggedIn={isLoggedIn} />
+      {button}
+    </div>
+  );
+}
+
+export default App;
