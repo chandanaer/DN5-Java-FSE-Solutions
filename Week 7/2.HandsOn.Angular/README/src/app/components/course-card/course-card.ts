26c64cacb9c7294574a9c172edb2029291dfa784
chandanaer <chandanaer9@gmail.com>
2026-07-28 19:11:21 +0530

    Add files via upload

---
+++ Week 7/2.HandsOn.Angular/README/src/app/components/course-card/course-card.ts
@@ -0,0 +1,31 @@
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-course-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-card.html',
+  styleUrl: './course-card.css'
+})
+export class CourseCard implements OnInit, OnChanges, OnDestroy {
+
+  @Input() course: any;
+
+  @Output() enrollRequested = new EventEmitter<number>();
+
+  ngOnInit() {
+    console.log('CourseCard initialized');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('Course changed:', changes);
+  }
+
+  ngOnDestroy() {
+    console.log('CourseCard destroyed');
+  }
+
+  enroll() {
+    this.enrollRequested.emit(this.course.id);
+  }
+}
---
+++ Week 7/2.HandsOn.Angular/README/src/app/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+  portalName = 'Student Course Portal';
+
+  isPortalActive = true;
+
+  message = '';
+  // Property binding is one-way (component → DOM).
+// ngModel provides two-way binding (DOM ↔ component).
+
+  searchTerm = '';
+
+  onEnrollClick() {
+    this.message = 'Enrollment opened!';
+  }
+
+}
