d4d23acf457399816823ab970e6fb231b9b1b6dc
chandanaer <chandanaer9@gmail.com>
2026-07-28 19:14:18 +0530

    Add files via upload

---
+++ Week 7/2.HandsOn.Angular/src/app/pages/course-list/course-list.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseCard } from '../../components/course-card/course-card';
+
+@Component({
+  selector: 'app-course-list',
+  standalone: true,
+  imports: [CommonModule, CourseCard],
+  templateUrl: './course-list.html',
+  styleUrl: './course-list.css'
+})
+export class CourseList {
+
+  selectedCourseId: number | null = null;
+
+  courses = [
+    { id: 1, name: 'Angular', code: 'ANG101', credits: 4 },
+    { id: 2, name: 'Java', code: 'JAVA201', credits: 3 },
+    { id: 3, name: 'Spring Boot', code: 'SPR301', credits: 4 }
+  ];
+
+  onEnroll(courseId: number) {
+    console.log('Enrolling in course:', courseId);
+    this.selectedCourseId = courseId;
+  }
+}
